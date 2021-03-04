ead74bbc9339f1b805d5a216dff49d5082fdfd07
georgy93 <georgy.georgiev93@gmail.com>
2021-03-04 12:23:02 +0200

    initial commit

---
+++ ConsoleApp5/IRestInterface.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp5
+{
+    extern alias MyAlias1;
+
+    using MyAlias1::ClassLibrary1;
+    using Refit;
+    using System.Threading.Tasks;
+
+    public interface IRestInterface
+    {
+        [Get("/someEndPoint")]
+        Task<Response> GetData(Request request);
+    }
+}
