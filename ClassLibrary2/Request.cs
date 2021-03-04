0887f28d68f95f2a6d61953c9c12accea3267ac4
georgy93 <georgy.georgiev93@gmail.com>
2021-03-04 12:29:14 +0200

    save

--- ClassLibrary2/Request.cs
+++ ClassLibrary2/Request.cs
@@ -1,4 +1,6 @@
-﻿namespace ClassLibrary2
+﻿// For some reason the other team decided to have identical namespaces in different nuggets, which I consume.
+// Thus, we have have duplicate unique class names.
+namespace ClassLibrary1  
 {
     public class Request
     {
